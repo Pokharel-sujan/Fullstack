065fe6fae44399e7ec364ba1dea59d8e930f34ee
Sujan <pokharelsujan134@gmail.com>
2019-05-09 14:42:18 +0300

    adding routes with exact things

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,9 @@ import ReactDOM from 'react-dom';
 import {Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 
-import App from './components/App'
-const store = createStore(() => [ ],{}, applyMiddleware() )    // middleware
+import App from './components/App';
+import reducers from './reducers';
+const store = createStore(() => [ ],{}, applyMiddleware() )    // middleware invoked with ()
 
 
-ReactDOM.render(<provider store ={store}> <App/> </provider>, document.querySelector('#root'));
+ReactDOM.render(<Provider store ={store}> <App/> </Provider>, document.querySelector('#root'));
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,6 @@
+export default function(state = { },action){    // initial state supposed to be empty object
+    switch (action.type){
+        default:
+        return state;
+    }
+}
