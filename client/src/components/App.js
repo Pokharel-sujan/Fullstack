93a5148bbe1e03c679940ff0a911c92f8cc5b09e
Sujan <pokharelsujan134@gmail.com>
2019-05-11 02:23:13 +0300

    wiring up header section to backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';  // helpers for navigating around Dom, other is (native and core-react-Library)
+import {connect }from 'react-redux';
+import * as actions from '../actions';
 
-// Dummy components
-const Header =() =><h2>Header</h2>
+// const Header =() =><h2>Header</h2> (//dummy component for down content)
+import Header from './Header';
 const Dashboard =() =><h2>Dashboard</h2>
 const SurveyNew =() =><h2>SurveyNew</h2>
 const Landing =() =><h2>Landing</h2>
 
-const App = ()=>{
-    return(
-        <div>
-            <BrowserRouter> 
+class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+        
+    }
+    render (){
+        return(
+         <div className = 'container'>
+         <BrowserRouter> 
             <div>
+                <Header/>   
                 <Route exact path ='/' component ={Landing} />
                 <Route exact path ='/surveys'component ={Dashboard} />
                 <Route path ='/surveys/new'component ={SurveyNew} />
@@ -19,6 +27,7 @@ const App = ()=>{
             </BrowserRouter>
         </div>
     );
-};
+ };
+}
 
-export default App;   
+export default connect(null,actions) (App);   //use of connect 
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,13 @@
-export default function(state = { },action){    // initial state supposed to be empty object
+import {FETCH_USER} from '../actions/types';
+
+
+
+export default function(state = null,action){    // initial state supposed to be empty object
+    
     switch (action.type){
+        case FETCH_USER: // fetch_user is action
+            return action.payload || false;
         default:
-        return state;
+            return state;
     }
 }
